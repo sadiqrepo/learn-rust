9de77a8fd0b628c5ef4cb1a156ada74e90bddca1
sadiqrepo <sadiq.n11@gmail.com>
2023-03-10 14:31:57 +0530

    restructuring

---
+++ rust_book/common_concepts/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/shaiksadiqahmed.ejas/gitprojects/learn-rust/gettingstarted/variables/target/debug/variables: /Users/shaiksadiqahmed.ejas/gitprojects/learn-rust/gettingstarted/variables/src/main.rs
