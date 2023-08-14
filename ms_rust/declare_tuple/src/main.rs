85af18ef8ae86bcbaefc1f87ecc85f7812ad3acb
sadiqrepo <sadiq.n11@gmail.com>
2023-08-14 11:39:31 +0530

    declare tuples

---
+++ ms_rust/declare_tuple/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    
+    //Declare a tuple of three elements
+    let tuple_e = ('E', 5i32, true);
+
+    //Use tuple indexing and access the values of the elements in the tuple
+    println!("Is '{}' the {}th letter of the alphabet? {}", tuple_e.0, tuple_e.1, tuple_e.2);
+}
